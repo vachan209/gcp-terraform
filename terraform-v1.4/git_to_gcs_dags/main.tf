94b353137017ba38d5a0dfde41409755cbc39c1f
vachan gowda <vachang9@gmail.com>
2023-05-02 18:07:48 +0530

    new code chnages

--- terraform-v1.4/git_to_gcs_dags/main.tf
+++ terraform-v1.4/git_to_gcs_dags/main.tf
@@ -3,8 +3,26 @@ data "google_composer_environment" "my_environment" {
     region="asia-east2"
 }
 
-resource "null_resource" "copy_file_to_bucket"{
-    provisioner "local-exec" {
-    command = "gsutil cp dags/* ${data.google_composer_environment.my_environment.config[0].dag_gcs_prefix}"    
-    }
+resource "null_resource" "copy_file_to_bucket" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      #!/bin/bash
+      set -euo pipefail
+
+      # Activate the service account
+      /home/ec2-user/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file=${var.gcp_creds_path}/credentials.json
+
+      # Set the active project
+      /home/ec2-user/google-cloud-sdk/bin/gcloud config set project kedila-gcp-dev
+
+      # Copy files to the Google Cloud Storage bucket
+      /home/ec2-user/google-cloud-sdk/bin/gsutil cp dags/* ${data.google_composer_environment.my_environment.config[0].dag_gcs_prefix}
+    EOT
+  }
 }
+
+
+
+
+
+
